fd767b97763cbd5c7c6fac256eaca0dfda82a467
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2017-11-23 14:39:52 +0100

    post

---
+++ y2p2programming1/Assets/NPC.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC : MonoBehaviour {
+
+    public Transform target;
+    public Transform item;
+    public float timer;
+
+    public enum Task { Roam, Collect, Dance, Idle };
+    public Task myTask;
+    public List<GameObject> objectives = new List<GameObject>();
+    
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        target = objectives[Random.Range(0, objectives.Count)].transform;
+        agent.SetDestination(target.transform.position);
+    }
+
+    void Update()
+    {
+        if (myTask == Task.Roam)
+        {
+            if (timer >= 2.5f)
+            {
+                timer = 0;
+                target = objectives[Random.Range(0, objectives.Count)].transform;
+                agent.SetDestination(target.transform.position);
+            }
+            else
+            {
+                timer = timer + Time.deltaTime;
+            }
+        }
+
+        if (myTask == Task.Collect)
+        {
+            if(item == null)
+            {
+                //FindCollectable();
+            }
+
+
+        }
+    }
+    void OnCollisionStay(Collision other)
+    {
+        if (myTask == Task.Collect)
+        {
+            if (other.gameObject.tag == "Collectable")
+            {
+                print("Collected");
+                Destroy(other.gameObject);
+                item = null;
+            }
+        }
+    }
+}
